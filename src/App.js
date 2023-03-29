7d66d97ace938237cbe369a9dd0da4be4dc084ce
Alexander <alexandernedrud@gmail.com>
2023-03-29 18:41:00 +0200

    Styling

--- src/App.js
+++ src/App.js
@@ -4,54 +4,58 @@ import Movies from './components/Movies';
 import Search from './components/Search';
 
 function App() {
-const [movies, setMovies] = useState([]);
-const [search, setSearch] = useState('');
-const title = 'James Bond';
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState('');
+  const title = 'James Bond';
 
-const fetchMovies = async (title) => {
-  const url = `http://www.omdbapi.com/?s=${title}&type=movie&plot=full&apikey=99468b8b&r=json`;
-  const response = await fetch(url);
-  const responseJson = await response.json();
-  if(responseJson.Search){
-    const movieInfo = await Promise.all(
-      responseJson.Search.map(async (movie) => {
-        const movieUrl = `http://www.omdbapi.com/?i=${movie.imdbID}&plot=full&apikey=25dbba7e&r=json`;
-        const movieResponse = await fetch(movieUrl);
-        const movieResponseJson = await movieResponse.json();
-        return {...movie, ...movieResponseJson}
-      })
-    );
-    setMovies(movieInfo);
-  }
-};
+  const fetchMovies = async (title) => {
+    const url = `http://www.omdbapi.com/?s=${title}&type=movie&plot=full&apikey=99468b8b&r=json`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    if (responseJson.Search) {
+      const movieInfo = await Promise.all(
+        responseJson.Search.map(async (movie) => {
+          const movieUrl = `http://www.omdbapi.com/?i=${movie.imdbID}&plot=full&apikey=25dbba7e&r=json`;
+          const movieResponse = await fetch(movieUrl);
+          const movieResponseJson = await movieResponse.json();
+          return { ...movie, ...movieResponseJson }
+        })
+      );
+      setMovies(movieInfo);
+    }
+  };
 
-const fetchMoviesSearch = async (search) => {
-  const url = `http://www.omdbapi.com/?s=${search}&type=movie&plot=full&apikey=99468b8b&r=json`;
-  const response = await fetch(url);
-  const responseJson = await response.json();
-  if(responseJson.Search){
-    const movieInfo = await Promise.all(
-      responseJson.Search.map(async (movie) => {
-        const movieUrl = `http://www.omdbapi.com/?i=${movie.imdbID}&plot=full&apikey=25dbba7e&r=json`;
-        const movieResponse = await fetch(movieUrl);
-        const movieResponseJson = await movieResponse.json();
-        return {...movie, ...movieResponseJson}
-      })
-    );
-    setMovies(movieInfo);
-  }
-};
+  const fetchMoviesSearch = async (search) => {
+    const url = `http://www.omdbapi.com/?s=${search}&type=movie&plot=full&apikey=99468b8b&r=json`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    if (responseJson.Search) {
+      const movieInfo = await Promise.all(
+        responseJson.Search.map(async (movie) => {
+          const movieUrl = `http://www.omdbapi.com/?i=${movie.imdbID}&plot=full&apikey=25dbba7e&r=json`;
+          const movieResponse = await fetch(movieUrl);
+          const movieResponseJson = await movieResponse.json();
+          return { ...movie, ...movieResponseJson }
+        })
+      );
+      setMovies(movieInfo);
+    }
+  };
 
-useEffect(() => {
-fetchMovies(title);
-fetchMoviesSearch(search);
-}, [search]);
-console.log('movies:', movies)
+  useEffect(() => {
+    fetchMovies(title);
+    fetchMoviesSearch(search);
+  }, [search]);
+  console.log('movies:', movies)
   return (
     <div className="App">
-      <Search search={search} setSearch={setSearch}></Search>
-      <h1>Movies</h1>
-      <Movies movies={movies}></Movies>
+      <main>
+        <Search search={search} setSearch={setSearch}></Search>
+        <h1 className="width">Recommended movies</h1>
+        <section className="width">
+          <Movies movies={movies}></Movies>
+        </section>
+      </main>
     </div>
   );
 };
